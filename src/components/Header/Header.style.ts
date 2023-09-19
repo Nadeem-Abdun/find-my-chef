0e36e94becb8aab1573e23f93757f647ffc44071
Nadeem Abdun <nadeem.abdun@bluepal.com>
2023-09-19 18:17:58 +0530

    Favicon Changed

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,5 +1,5 @@
 // import { styled } from '@mui/system';
-import { Typography, Box } from '@mui/material'
+import { Box } from '@mui/material'
 import styled from 'styled-components';
 
 // export const NavPaddingX = styled(Box)`
